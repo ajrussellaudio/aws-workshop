61847be560dfaa83524636d95f699363e552ce87
Alan Russell <ajrussellaudio@gmail.com>
2025-07-11 10:48:03 +0100

    refactor: Unify Lambda handlers into a single project
    This commit refactors the five separate Lambda handlers (create, read, read-all, update, delete) into a single, unified project under `src/services`.
    
    Key changes:
    - Moved all handler logic into individual `.ts` files within `src/services`.
    - Consolidated all dependencies into a single `package.json`.
    - Created a single `tsconfig.json` for the unified project.
    - Updated the `lambda.tf` to reflect the new handler locations and build process.
    - Excluded `aws-sdk` from the build to reduce bundle size.
    
    This simplifies the project structure, streamlines dependency management, and improves the build process.

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -9,20 +9,20 @@ resource "null_resource" "build_lambdas" {
   }
 }
 
-data "archive_file" "read_all_lambda_zip" {
+data "archive_file" "lambda_zip" {
   type        = "zip"
-  source_dir  = "${path.module}/../src/services/read-all/dist"
-  output_path = "${path.module}/../src/services/read-all/dist/handler.zip"
+  source_dir  = "${path.module}/../src/services/dist"
+  output_path = "${path.module}/../src/services/dist/handler.zip"
   depends_on = [null_resource.build_lambdas]
 }
 
 resource "aws_lambda_function" "read_all_item" {
   function_name = "aws-workshop-read_all_item"
   role          = aws_iam_role.lambda_exec_role.arn
-  handler       = "handler.handler"
+  handler       = "read-all.handler"
   runtime       = "nodejs16.x"
-  filename      = data.archive_file.read_all_lambda_zip.output_path
-  source_code_hash = data.archive_file.read_all_lambda_zip.output_base64sha256
+  filename      = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 
   environment {
     variables = {
@@ -39,20 +39,13 @@ resource "aws_lambda_permission" "apigw_lambda_read_all" {
   source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/${aws_api_gateway_stage.api_stage.stage_name}/${aws_api_gateway_method.read_all_item_method.http_method}${aws_api_gateway_resource.items.path}"
 }
 
-data "archive_file" "create_lambda_zip" {
-  type        = "zip"
-  source_dir  = "${path.module}/../src/services/create/dist"
-  output_path = "${path.module}/../src/services/create/dist/handler.zip"
-  depends_on = [null_resource.build_lambdas]
-}
-
 resource "aws_lambda_function" "create_item" {
   function_name = "aws-workshop-create_item"
   role          = aws_iam_role.lambda_exec_role.arn
-  handler       = "handler.handler"
+  handler       = "create.handler"
   runtime       = "nodejs16.x"
-  filename      = data.archive_file.create_lambda_zip.output_path
-  source_code_hash = data.archive_file.create_lambda_zip.output_base64sha256
+  filename      = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 
   environment {
     variables = {
@@ -69,20 +62,13 @@ resource "aws_lambda_permission" "apigw_lambda_create" {
   source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/${aws_api_gateway_stage.api_stage.stage_name}/${aws_api_gateway_method.create_item_method.http_method}${aws_api_gateway_resource.items.path}"
 }
 
-data "archive_file" "read_lambda_zip" {
-  type        = "zip"
-  source_dir  = "${path.module}/../src/services/read/dist"
-  output_path = "${path.module}/../src/services/read/dist/handler.zip"
-  depends_on = [null_resource.build_lambdas]
-}
-
 resource "aws_lambda_function" "read_item" {
   function_name = "aws-workshop-read_item"
   role          = aws_iam_role.lambda_exec_role.arn
-  handler       = "handler.handler"
+  handler       = "read.handler"
   runtime       = "nodejs16.x"
-  filename      = data.archive_file.read_lambda_zip.output_path
-  source_code_hash = data.archive_file.read_lambda_zip.output_base64sha256
+  filename      = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 
   environment {
     variables = {
@@ -99,20 +85,13 @@ resource "aws_lambda_permission" "apigw_lambda_read" {
   source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/${aws_api_gateway_stage.api_stage.stage_name}/${aws_api_gateway_method.read_item_method.http_method}${aws_api_gateway_resource.item.path}"
 }
 
-data "archive_file" "update_lambda_zip" {
-  type        = "zip"
-  source_dir  = "${path.module}/../src/services/update/dist"
-  output_path = "${path.module}/../src/services/update/dist/handler.zip"
-  depends_on = [null_resource.build_lambdas]
-}
-
 resource "aws_lambda_function" "update_item" {
   function_name = "aws-workshop-update_item"
   role          = aws_iam_role.lambda_exec_role.arn
-  handler       = "handler.handler"
+  handler       = "update.handler"
   runtime       = "nodejs16.x"
-  filename      = data.archive_file.update_lambda_zip.output_path
-  source_code_hash = data.archive_file.update_lambda_zip.output_base64sha256
+  filename      = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 
   environment {
     variables = {
@@ -129,20 +108,13 @@ resource "aws_lambda_permission" "apigw_lambda_update" {
   source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/${aws_api_gateway_stage.api_stage.stage_name}/${aws_api_gateway_method.update_item_method.http_method}${aws_api_gateway_resource.item.path}"
 }
 
-data "archive_file" "delete_lambda_zip" {
-  type        = "zip"
-  source_dir  = "${path.module}/../src/services/delete/dist"
-  output_path = "${path.module}/../src/services/delete/dist/handler.zip"
-  depends_on = [null_resource.build_lambdas]
-}
-
 resource "aws_lambda_function" "delete_item" {
   function_name = "aws-workshop-delete_item"
   role          = aws_iam_role.lambda_exec_role.arn
-  handler       = "handler.handler"
+  handler       = "delete.handler"
   runtime       = "nodejs16.x"
-  filename      = data.archive_file.delete_lambda_zip.output_path
-  source_code_hash = data.archive_file.delete_lambda_zip.output_base64sha256
+  filename      = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 
   environment {
     variables = {
